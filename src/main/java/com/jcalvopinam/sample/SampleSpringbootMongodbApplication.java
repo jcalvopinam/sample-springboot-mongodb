ea26b0308414e2585b7db54d064d6741fb6b43b9
Juan Calvopina <juan.calvopina@gmail.com>
2018-04-17 15:57:11 -0300

    Initial commit

---
+++ src/main/java/com/jcalvopinam/sample/SampleSpringbootMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.jcalvopinam.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringbootMongodbApplication.class, args);
+    }
+}

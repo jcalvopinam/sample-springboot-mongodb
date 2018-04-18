82643814cb7a51ea8f44bf4c5d5ad14b8538b181
Juan Calvopina <juan.calvopina@gmail.com>
2018-04-18 10:03:40 -0300

    Adds domain classes

---
+++ src/main/java/com/jcalvopinam/sample/domain/Restaurant.java
@@ -0,0 +1,80 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 JUAN CALVOPINA M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.jcalvopinam.sample.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "Restaurants")
+public class Restaurant {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(direction = IndexDirection.ASCENDING)
+    private int pricePerDinner;
+
+    private Address address;
+
+    private List<Review> reviews;
+
+    protected Restaurant() {
+        this.reviews = new ArrayList<>();
+    }
+
+    public Restaurant(String name, int pricePerDinner, Address address, List<Review> reviews) {
+        this.name = name;
+        this.pricePerDinner = pricePerDinner;
+        this.address = address;
+        this.reviews = reviews;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPricePerDinner() {
+        return pricePerDinner;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+}
